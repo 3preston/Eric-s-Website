907e6e9a1dae052bd0151fa7262b1b02945b138c
Preston <prest114@msu.edu>
2022-05-13 22:27:59 -0400

    twitter working well

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -25,4 +25,8 @@ export class HomePage {
     this.chosenSegment = chosenSegment.detail.value;
   }
 
+  follow() {
+   window.open("https://twitter.com/intent/follow?original_referer=http%3A%2F%2Flocalhost%3A8100%2F&ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Efollow%7Ctwgr%5EericprestonETH&region=follow_link&screen_name=ericprestonETH") 
+  }
+
 }
